4da4ed046b045087eecb0885513116c906b7c5c0
olinykFs <oleh.olinyk.oo@gmail.com>
2024-11-02 18:04:52 +0100

    upd

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module org.example.javafxtest {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens org.example.javafxtest to javafx.fxml;
+    exports org.example.javafxtest;
+}
