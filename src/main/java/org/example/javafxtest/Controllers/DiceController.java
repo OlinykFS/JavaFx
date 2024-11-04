caafb12735a7ee513b182b2b23b78ad265735409
olinykFs <oleh.olinyk.oo@gmail.com>
2024-11-04 20:49:26 +0100

    upd

--- src/main/java/org/example/javafxtest/Controllers/DiceController.java
+++ src/main/java/org/example/javafxtest/Controllers/DiceController.java
@@ -1,4 +1,11 @@
 package org.example.javafxtest.Controllers;
 
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Label;
+
 public class DiceController {
+    FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("@/org/example/javafxtest/Views/DiceView.fxml"));
+    @FXML
+    private Label label;
 }
---
+++ src/main/java/org/example/javafxtest/Routers/SceneRouter.java
@@ -0,0 +1,4 @@
+package org.example.javafxtest.Routers;
+
+public class SceneRouter {
+}
