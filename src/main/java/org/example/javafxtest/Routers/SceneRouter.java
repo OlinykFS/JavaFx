7186e185e84ecce081ca6d903d4762ef880b38da
olinykFs <oleh.olinyk.oo@gmail.com>
2024-11-04 20:59:12 +0100

    upd

--- src/main/java/org/example/javafxtest/Routers/SceneRouter.java
+++ src/main/java/org/example/javafxtest/Routers/SceneRouter.java
@@ -1,4 +1,42 @@
 package org.example.javafxtest.Routers;
 
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class SceneRouter {
+    private static SceneRouter instance;
+    private Stage primaryStage;
+    private SceneRouter() {
+
+    }
+    private static SceneRouter getInstance(){
+        if(instance == null){
+            instance = new SceneRouter();
+        }
+        return instance;
+    }
+
+    public void SetPrimaryStage(Stage stage){
+        this.primaryStage = stage;
+    }
+    public void SwitchToMenu(){
+        switchScene("Views/Menu-View.fxml", "Menu");
+    }
+    public void SwitchToCalculator(){
+        switchScene("Views/Calculator-View.fxml", "Calculator");
+    }
+    private void switchScene(String fxmlPath, String title){
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource(fxmlPath));
+            Scene scene = new Scene(fxmlLoader.load(), 400,500);
+            primaryStage.setScene(scene);
+            primaryStage.setTitle(title);
+            primaryStage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
